1e02768a4da5029278a8ca7dd995081c84ceab82
Ruslan Zulufov <gothiq2302@gmail.com>
2023-04-21 14:03:38 +0600

    Создание ограничений к api. Обновление фикстур

--- instagram/api/urls.py
+++ instagram/api/urls.py
@@ -1,6 +1,7 @@
 from api.views import PostView, LikeView
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
 router.register('posts', PostView)
@@ -8,4 +9,5 @@ router.register('likes', LikeView)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login/', obtain_auth_token, name='obtain_auth_token'),
 ]
--- instagram/api/views.py
+++ instagram/api/views.py
@@ -1,19 +1,19 @@
-from django.contrib.auth import get_user_model
+from api.serializers import PostSerializer, LikeSerializer
+from posts.models import Like
+from posts.models import Post
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
-from posts.models import Post
-
-from api.serializers import PostSerializer, LikeSerializer
-
-from posts.models import Like
+from api.permissions import PostPermission
 
 
 class PostView(ModelViewSet):
+    permission_classes = [PostPermission, IsAuthenticatedOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class LikeView(ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
-
